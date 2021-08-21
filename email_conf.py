bb83e5796fd0fe4f692be97c4d9867b368254783
ranivenigalla <87524280+ranivenigalla@users.noreply.github.com>
2021-08-21 18:31:53 +0530

    Create email_conf.py

---
+++ email_conf.py
@@ -0,0 +1,6 @@
+MAILGUN_API_KEY = 'This is the private API key which you can find on your Mailgun Dashboard' 
+SANDBOX_URL= 'You can find this on your Mailgun Dashboard' 
+SENDER_EMAIL = 'test@' + SANDBOX_URL  # No need to modify this. The sandbox URL is of the format test@YOUR_SANDBOX_URL
+RECIPIENT_EMAIL = 'Enter your Email ID Here'
+API_KEY = 'This is your Bolt Cloud account API key'
+DEVICE_ID = 'This is the ID of your Bolt device
